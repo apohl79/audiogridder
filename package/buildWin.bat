3e46235d5a8a0ab8fbffaedad923c48ba7e95fa5
Andreas Pohl <audiogridder@e47.org>
2021-04-13 10:05:56 +0200

    Removed cmake parallel param for Windows build

--- package/buildWin.bat
+++ package/buildWin.bat
@@ -1,7 +1,7 @@
 cd ..
 del /F /S /Q build-win-10-x86_64
 cmake -B build-win-10-x86_64 -A x64 -DFFMPEG_ROOT=z:/ag-deps-win-x86_64
-cmake --build build-win-10-x86_64 --config RelWithDebInfo -j6
+cmake --build build-win-10-x86_64 --config RelWithDebInfo
 
 cd package
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Obuild AudioGridderPlugin.iss
